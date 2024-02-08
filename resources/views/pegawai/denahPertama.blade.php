4f2487f479b7a6f0f9b377932e07bcf7727ba715
Marchell22 <ramotmanurung424@gmail.com>
2024-02-09 02:18:22 +0700

    new

--- resources/views/pegawai/denahPertama.blade.php
+++ resources/views/pegawai/denahPertama.blade.php
@@ -37,6 +37,53 @@
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('lte/plugins/summernote/summernote-bs4.min.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" />
+    <style>
+        /* Tambahkan CSS untuk styling modal */
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.7);
+        }
+
+        /* Style untuk konten modal */
+        .modal-content {
+            background-color: #fefefe;
+            margin: 10% auto; /* Mengurangi margin agar modal lebih muncul di tengah */
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+            max-height: 80vh; /* Menentukan tinggi maksimum modal sesuai dengan tinggi layar */
+            overflow-y: auto; /* Menambahkan overflow-y agar dapat di-scroll jika kontennya lebih panjang */
+        }
+
+        /* Style untuk tombol close */
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+        @media (min-width: 768px) {
+            .modal-content {
+                width: 50%; /* Mengubah lebar modal pada layar yang lebih besar */
+                margin-left: 30%; 
+            }
+        }
+
+    </style>
 
 </head>
 
@@ -152,175 +199,181 @@
             <div class="entahlah-transformed-parent">
                 <img class="entahlah-transformed-icon" alt="" src="{{ asset('img/denahPertama.png') }}" />
                 <div class="pi-1022-wrapper">
-                    <b class="pi-1020b">35-PI-1022</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pi-1020b">35-PI-1022</b></a>
                 </div>
                 <div class="ti-1022-wrapper">
-                    <b class="pi-1020b">35-TI-1022</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pi-1020b">35-TI-1022</b></a>
                 </div>
                 <div class="bv-1022-wrapper">
-                    <b class="bv-1022">35-BV-1022</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-1022">35-BV-1022</b></a>
                 </div>
                 <div class="pt-1031a-wrapper">
-                    <b class="pt-1031a">35-PT-1031A</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pt-1031a">35-PT-1031A</b></a>
                 </div>
                 <div class="pt-1031b-wrapper">
-                    <b class="pt-1031a">35-PT-1031B</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pt-1031a">35-PT-1031B</b></a>
                 </div>
                 <div class="pt-1031c-wrapper">
-                    <b class="pt-1031a">35-PT-1031C</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pt-1031a">35-PT-1031C</b></a>
                 </div>
                 <div class="tt-1020-wrapper">
-                    <b class="tt-1020">35-TT-1020</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="tt-1020">35-TT-1020</b></a>
                 </div>
                 <div class="ti-1020-wrapper">
-                    <b class="tt-1020">35-TI-1020</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="tt-1020">35-TI-1020</b></a>
                 </div>
                 <div class="plv-3005-wrapper">
-                    <b class="pt-1031a">35-PLV-3005</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pt-1031a">35-PLV-3005</b></a>
                 </div>
                 <div class="plv-3006-wrapper">
-                    <b class="plv-3006">35-PLV-3006</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PLV-3006</b></a>
                 </div>
                 <div class="gtv-1401-wrapper">
-                    <b class="gtv-1401">35-GTV-1401</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="gtv-1401">35-GTV-1401</b></a>
                 </div>
                 <div class="plv-1401-wrapper">
-                    <b class="plv-3006">35-PLV-1401</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PLV-1401</b></a>
                 </div>
                 <div class="plv-1002-wrapper">
-                    <b class="plv-3006">35-PLV-1002</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PLV-1002</b></a>
                 </div>
                 <div class="plv-1003-wrapper">
-                    <b class="plv-3006">35-PLV-1003</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PLV-1003</b></a>
                 </div>
                 <div class="zsc-1025-wrapper">
-                    <b class="plv-3006">35-ZSC-1025</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-ZSC-1025</b></a>
                 </div>
                 <div class="bv-1401-wrapper">
-                    <b class="gtv-1401">35-BV-1401</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="gtv-1401">35-BV-1401</b></a>
                 </div>
                 <div class="bv-1402-wrapper">
-                    <b class="bv-1402">
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-1402">
                         <p class="p">35</p>
                         <p class="p">BV</p>
                         <p class="p">1402</p>
-                    </b>
+                    </b></a>
                 </div>
                 <div class="plv-1401-container">
-                    <b class="plv-3006">35-PLV-1401</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PLV-1401</b></a>
                 </div>
                 <div class="psv-1020-wrapper">
-                    <b class="plv-3006">35-PSV-1020</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PSV-1020</b></a>
                 </div>
                 <div class="pi-1020b-wrapper">
-                    <b class="pi-1020b">35-PI-1020B</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pi-1020b">35-PI-1020B</b></a>
                 </div>
                 <div class="pi-1020b-wrapper">
-                    <b class="pi-1020b">35-PI-1020B</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pi-1020b">35-PI-1020B</b></a>
                 </div>
                 <div class="bv-1002-wrapper">
-                    <b class="gtv-1401">35-BV-1002</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="gtv-1401">35-BV-1002</b></a>
                 </div>
                 <div class="bv-1002-container">
-                    <b class="gtv-1401">35-BV-1002</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="gtv-1401">35-BV-1002</b></a>
                 </div>
                 <div class="zs-1021-wrapper">
-                    <b class="gtv-1401">35-ZS-1021</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="gtv-1401">35-ZS-1021</b></a>
                 </div>
                 <div class="hcv-1020-wrapper">
-                    <b class="hcv-1020">35-HCV-1020</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="hcv-1020">35-HCV-1020</b></a>
                 </div>
                 <div class="bv-1002-frame">
-                    <b class="bv-10022">35-BV-1002</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-10022">35-BV-1002</b></a>
                 </div>
                 <div class="bv-1001-wrapper">
-                    <b class="gtv-1401">35-BV-1001</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="gtv-1401">35-BV-1001</b></a>
                 </div>
                 <div class="bv-1403-wrapper">
-                    <b class="bv-1403">35-BV-1403</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-1403">35-BV-1403</b></a>
                 </div>
                 <div class="plv-1402-wrapper">
-                    <b class="plv-1402">35-PLV-1402</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-1402">35-PLV-1402</b></a>
                 </div>
                 <div class="blowdown-to-atm-wrapper">
-                    <b class="blowdown-to-atm">BLOWDOWN TO ATM</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="blowdown-to-atm">BLOWDOWN TO ATM</b></a>
                 </div>
                 <div class="pi-1025-wrapper">
-                    <b class="pi-1025">35-PI-1025</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pi-1025">35-PI-1025</b></a>
                 </div>
                 <div class="bv-3002-wrapper">
-                    <b class="bv-3002">35-BV-3002</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-3002">35-BV-3002</b></a>
                 </div>
                 <div class="bv-3001-wrapper">
-                    <b class="bv-3002">35-BV-3001</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-3002">35-BV-3001</b></a>
                 </div>
                 <div class="pt-1021-wrapper">
-                    <b class="bv-3002">35-PT-1021</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-3002">35-PT-1021</b></a>
                 </div>
                 <div class="zs-1020-wrapper">
-                    <b class="bv-3002">35-ZS-1020</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-3002">35-ZS-1020</b></a>
                 </div>
                 <div class="plv-3003-wrapper">
-                    <b class="plv-3003">35-PLV-3003</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3003">35-PLV-3003</b></a>
                 </div>
                 <div class="bv-3005-wrapper">
-                    <b class="plv-3003">35-BV-3005</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3003">35-BV-3005</b></a>
                 </div>
                 <div class="hcv-1021-wrapper">
-                    <b class="plv-3003">35-HCV-1021</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3003">35-HCV-1021</b></a>
                 </div>
                 <div class="plv-3004-wrapper">
-                    <b class="plv-3003">35-PLV-3004</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3003">35-PLV-3004</b></a>
                 </div>
                 <div class="bv-3006-wrapper">
-                    <b class="plv-3003">35-BV-3006</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3003">35-BV-3006</b></a>
                 </div>
                 <div class="frame-div">
-                    <b class="bv-10023">35-BV-1002</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-10023">35-BV-1002</b></a>
                 </div>
                 <div class="bv-1002-wrapper1">
-                    <b class="bv-10023">35-BV-1002</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="bv-10023">35-BV-1002</b></a>
                 </div>
                 <div class="plv-1001-wrapper">
-                    <b class="plv-3006">35-PLV-1001</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PLV-1001</b></a>
                 </div>
                 <div class="pi-1020a-wrapper">
-                    <b class="pi-1020b">35-PI-1020A</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="pi-1020b">35-PI-1020A</b></a>
                 </div>
                 <div class="r-1020-wrapper">
-                    <b class="r-1020">35-R-1020</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="r-1020">35-R-1020</b></a>
                 </div>
                 <div class="vent-atm-wrapper">
-                    <b class="vent-atm">
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="vent-atm">
                         <p class="p">VENT</p>
                         <p class="p">ATM</p>
-                    </b>
+                    </b></a>
                 </div>
                 <div class="to-sump-tank-parent">
-                    <b class="to-sump-tank">TO SUMP TANK</b>
-                    <b class="v-2140">35-V-2140</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="to-sump-tank">TO SUMP TANK</b></a>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="v-2140">35-V-2140</b></a>
                 </div>
                 <div class="to-sump-tank-parent">
-                    <b class="to-sump-tank">TO SUMP TANK</b>
-                    <b class="v-2140">35-V-2140</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="to-sump-tank">TO SUMP TANK</b></a>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="v-2140">35-V-2140</b></a>
                 </div>
                 <div class="to-pig-launcher-parent">
-                    <b class="to-pig-launcher">TO PIG LAUNCHER</b>
-                    <b class="l-1320">35-L-1320</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="to-pig-launcher">TO PIG LAUNCHER</b></a>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="l-1320">35-L-1320</b></a>
                 </div>
                 <div class="plv-3010-wrapper">
-                    <b class="plv-3006">35-PLV-3010</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3006">35-PLV-3010</b></a>
                 </div>
                 <div class="plv-3009-wrapper">
-                    <b class="plv-3009">35-PLV-3009</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3009">35-PLV-3009</b></a>
                 </div>
                 <div class="sdv-1023-wrapper">
-                    <b class="plv-3009">35-SDV-1023</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="plv-3009">35-SDV-1023</b></a>
                 </div>
                 <div class="from-pig-launcher-parent">
-                    <b class="from-pig-launcher">FROM PIG LAUNCHER</b>
-                    <b class="l-13201">33-L-1320</b>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="from-pig-launcher">FROM PIG LAUNCHER</b></a>
+                    <a href=""  class="show-modal"onclick="showModal()"><b class="l-13201">33-L-1320</b></a>
                 </div>
+            </div>
+        </div>
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close" onclick="closeModal()">&times;</span>
+                <p>HAHAHA</p>
             </div>
         </div>
 
@@ -370,6 +423,41 @@
     <script src="{{ asset ('lte/dist/js/demo.js')}}"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="{{ asset ('lte/dist/js/pages/dashboard.js')}}"></script>
+    <script>
+        // Temukan elemen-elemen yang diperlukan
+        var modal = document.getElementById('myModal');
+        var showModalBtns = document.getElementsByClassName('show-modal');
+        var span = document.getElementsByClassName('close')[0];
+
+        // Fungsi untuk menampilkan modal
+        function showModal(event) {
+            event.preventDefault(); // Menahan perilaku asli dari tautan
+            modal.style.display = 'block';
+        }
+
+        // Fungsi untuk menutup modal
+        function closeModal() {
+            modal.style.display = 'none';
+        }
+
+        // Tambahkan event listener ke setiap elemen <a>
+        for (var i = 0; i < showModalBtns.length; i++) {
+            showModalBtns[i].addEventListener('click', function(event) {
+                showModal(event);
+            });
+        }
+
+        // Tambahkan event listener ke tombol close
+        span.addEventListener('click', closeModal);
+
+        // Tambahkan event listener untuk menutup modal saat mengklik di luar modal
+        // window.addEventListener('click', function (event) {
+        //     if (event.target == modal) {
+        //         closeModal();
+        //     }
+        // });
+
+    </script>
 
 </body>
 
